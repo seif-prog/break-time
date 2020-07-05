890a564bca1815862db17608ec235e401415b4f6
seif <50073621+seif-prog@users.noreply.github.com>
2020-07-05 14:20:13 +0200

    Add files via upload

---
+++ breaktime.py
@@ -0,0 +1,11 @@
+import time
+
+i=0
+
+while i>=3:
+    time.sleep(60*15)
+    print ('Take a short break')
+    i+=1
+if i=3:
+    print ('You should take a long braek')
+    print ('You spent alot of time working give yourself a break :)')
