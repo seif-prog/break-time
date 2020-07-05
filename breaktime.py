400a6ae11fce30b3978e89ae2d226d8c50d8ad44
seif <50073621+seif-prog@users.noreply.github.com>
2020-07-05 14:56:45 +0200

    Update breaktime.py

--- breaktime.py
+++ breaktime.py
@@ -2,10 +2,11 @@ import time
 
 i=0
 
-while i>=3:
+while i < 4:
     time.sleep(60*15)
     print ('Take a short break')
     i+=1
-if i=3:
+if i is 3:
     print ('You should take a long braek')
     print ('You spent alot of time working give yourself a break :)')
+    time.sleep(60*10)
